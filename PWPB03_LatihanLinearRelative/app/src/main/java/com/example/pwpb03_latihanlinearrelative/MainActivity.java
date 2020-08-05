c44b0a711a502d1253678740ba22542944430413
Agus Rohendi <arohendi.siswa@smkn4bdg.sch.id>
2020-08-05 14:15:49 +0700

    Pertemuan 4

---
+++ PWPB03_LatihanLinearRelative/app/src/main/java/com/example/pwpb03_latihanlinearrelative/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pwpb03_latihanlinearrelative;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+    }
+}
